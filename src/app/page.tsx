68faa86edffab14686ba73364417d818566676f3
Tomas <150765761+thithada@users.noreply.github.com>
2025-02-06 14:15:02 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>superhero</li>
+          <li>66026055 Thithada nextjs2</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
